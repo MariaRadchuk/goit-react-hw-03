c2d6ae28832f5f9f971d6977443c7637f75c1547
MariaRadchuk <mradchuk_15@ukr.net>
2024-05-11 13:13:40 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import ContactForm from "./components/ContactForm.jsx";
-import SearchBox from "./components/SearchBox.jsx";
-import ContactList from "./components/ContactList.jsx";
-import Contact from "./components/Contact.jsx";
+import ContactForm from "./components/ContactForm/ContactForm.jsx";
+import SearchBox from "./components/SearchBox/SearchBox.jsx";
+import ContactList from "./components/ContactList/ContactList.jsx";
 
 function App() {
   const [contactDatas, setContactDatas] = useState(() => {
